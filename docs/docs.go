389a20dd8d245b4d2e3239424eb09d2394c7cb18
Zach Russell <zach@zachs.io>
2026-03-28 16:17:37 -0600

    docs: regenerate swagger with correct host port 8181

--- docs/docs.go
+++ docs/docs.go
@@ -304,7 +304,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "localhost:8181",
 	BasePath:         "/api/v1",
 	Schemes:          []string{},
 	Title:            "Voyage API",
